a01c2e44bcb1b89b3ea463eeef33dbe11dec2fea
Arun Marsten <arunmarsten@aruns-mbp.mynetworksettings.com>
2026-09-08 16:58:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# μZAK
+
+we made a webapp on a train with spotty wifi. relax
---
+++ index.js
@@ -0,0 +1,88 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+let x;
+
+let port = process.env.PORT || 8125;
+
+http.createServer(function (request, response) {
+    // Set CORS headers
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Request-Method', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    if ( request.method === 'OPTIONS' ) {
+        response.writeHead(200);
+        response.end();
+        return;
+    }
+    else if (request.url == "/input"){
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        request.on('end', () => {
+            console.log(body);
+            x = body;
+            process.stdout.write(body);
+            response.end('ok');
+        });
+    }
+    else if (request.url == "/output"){
+        response.writeHead(200, {'Content-Type':'application/json'});
+        response.write(JSON.stringify({output:x}));
+        response.end();
+    }
+    else {
+        console.log('request ', request.url);
+
+        let filePath = '.' + request.url;
+        if (filePath == './') {
+            filePath = './index.html';
+        }
+
+        let extname = String(path.extname(filePath)).toLowerCase();
+        const mimeTypes = {
+            '.html': 'text/html',
+            '.js': 'text/javascript',
+            '.css': 'text/css',
+            '.json': 'application/json',
+            '.png': 'image/png',
+            '.jpg': 'image/jpg',
+            '.gif': 'image/gif',
+            '.wav': 'audio/wav',
+            '.mp4': 'video/mp4',
+            '.woff': 'application/font-woff',
+            '.ttf': 'application/font-ttf',
+            '.eot': 'application/vnd.ms-fontobject',
+            '.otf': 'application/font-otf',
+            '.svg': 'application/image/svg+xml'
+        };
+
+        let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+        fs.readFile(filePath, function(error, content) {
+            if (error) {
+                if(error.code == 'ENOENT') {
+                    fs.readFile('./404.html', function(error, content) {
+                        response.writeHead(200, { 'Content-Type': contentType });
+                        response.end(content, 'utf-8');
+                    });
+                }
+                else {
+                    response.writeHead(500);
+                    response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                    response.end();
+                }
+            }
+            else {
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
+            }
+        });
+    }
+    
+
+}).listen(port);
+console.log('Server running at http://127.0.0.1:8125/');
